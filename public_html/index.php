4f8b37300d5cc298ed7332e238269d1b29f13ad7
Juan De Luna <pablo.dluna@hotmail.com>
2023-10-27 16:12:20 -0600

    Mejoras en index y footer

--- public_html/index.php
+++ public_html/index.php
@@ -11,10 +11,29 @@
 <body>
     <?php require "header.html" ?>
 
-    El eslogan es:  El software inteligente para gente inteligente <br>
-    Es chinga quedito y queda bonito
+    <p>
+        El software inteligente para gente inteligente
+    </p>
 
+    <section>
+        <h2>Desarrollo de software a medida</h2>
+        <p>En Softix nos especializamos en el desarrollo de software a medida para empresas de todos los tamaños. Nuestro equipo de expertos en programación trabaja en estrecha colaboración con nuestros clientes para crear soluciones personalizadas que satisfagan sus necesidades específicas.</p>
+    </section>
 
+    <section>
+        <h2>Desarrollo de aplicaciones móviles</h2>
+        <p>En la era de los smartphones, las aplicaciones móviles son una herramienta esencial para cualquier empresa. En Softix, desarrollamos aplicaciones móviles para iOS y Android que ayudan a nuestros clientes a llegar a su público objetivo de manera efectiva.</p>
+    </section>
+
+    <section>
+        <h2>Desarrollo de sitios web</h2>
+        <p>Un sitio web es la cara de su empresa en línea. En Softix, creamos sitios web atractivos y funcionales que ayudan a nuestros clientes a conectarse con su audiencia en línea. Ya sea que necesite un sitio web de comercio electrónico o un sitio web informativo, podemos ayudarlo.</p>
+    </section>
+
+    <section>
+        <h2>Servicios de mantenimiento y soporte</h2>
+        <p>En Softix, no solo desarrollamos software, sino que también brindamos servicios de mantenimiento y soporte para garantizar que su software funcione sin problemas. Ofrecemos servicios de mantenimiento preventivo y correctivo, así como soporte técnico para ayudarlo a resolver cualquier problema que pueda surgir.</p>
+    </section>
 
     <?php require "footer.html" ?>
 </body>
