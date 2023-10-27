1c8cdf58793cc32f43df03f7872c461c7233c163
Juan De Luna <pablo.dluna@hotmail.com>
2023-10-27 16:03:31 -0600

    Archivo .php del formulario y logo

--- public_html/vacanteForm.php
+++ public_html/vacanteForm.php
@@ -1,22 +1,18 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img\Logo.png" type="image/x-icon">
-    <title>Softix</title>
-
+    <title>Document</title>
 </head>
-
 <body>
+
     <?php require "header.html" ?>
 
-    El eslogan es:  El software inteligente para gente inteligente <br>
-    Es chinga quedito y queda bonito
+    jaja te redireccione
 
 
 
     <?php require "footer.html" ?>
 </body>
-
 </html>
