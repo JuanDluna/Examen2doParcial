dce8d82e4b4b8a1ea2bab73c09952cbbea20340f
Juan De Luna <pablo.dluna@hotmail.com>
2023-10-27 15:41:19 -0600

    Simple changes

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Softix</title>
+
+</head>
+
+<body>
+    <?php include "basic_php/header.php" ?>
+
+</body>
+
+</html>
