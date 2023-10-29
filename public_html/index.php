64eb10fce84736e59e4c7bf9a7b848e82432baf9
Ronalgon <al330056@edu.uaa.mx>
2023-10-28 20:40:01 -0600

    v2

--- public_html/index.php
+++ public_html/index.php
@@ -1,41 +1,265 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img\Logo.png" type="image/x-icon">
     <title>Softix</title>
+    
+    <!-- link javascript -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <!-- link css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- icono -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
 
 <body>
-    <?php require "header.html" ?>
 
-    <p>
-        El software inteligente para gente inteligente
-    </p>
+    <!-- header -->
+    <header class="header">
+        <a href="" class="logo">SOFTIX</a>
+        <nav class="navbar">
+            <a href="#home" class="active">Inicio</a>
+            <a href="#">Servicios</a>
+            <a href="#contacto">Contacto</a>
+            <a href="#">Registrate</a>
+        </nav>
 
-    <section>
-        <h2>Desarrollo de software a medida</h2>
-        <p>En Softix nos especializamos en el desarrollo de software a medida para empresas de todos los tamaños. Nuestro equipo de expertos en programación trabaja en estrecha colaboración con nuestros clientes para crear soluciones personalizadas que satisfagan sus necesidades específicas.</p>
+        <div class="bx bx-moon" id="darkMode-icon"></div>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </header>
+
+    <!--inicio home-->
+    <section class="home" id="home">
+        <div class="home-content">
+            <h3>Hola! Nosotros somos</h3>
+            <h1>SOFTIX</h1>
+            <p>Conoce todas las maravillas que tenemos para ti</p>
+    
+            <div class="social-media">
+                <a href=""><i class='bx bxl-facebook' ></i></a>
+                <a href=""><i class='bx bxl-twitter' ></i></a>
+                <a href=""><i class='bx bxl-instagram-alt' ></i></a>
+                <a href=""><i class='bx bxl-whatsapp' ></i></a>
+            </div>
+            <a href="#" class="btn">Iniciar Ingreso</a>
+        </div> 
+
+        <div class="profession-container">
+            <div class="profession-box">
+
+                <div class="profession" style="--i:0;">
+                <i class='bx bx-code-alt' ></i>
+                <h3>CAPACITATE!!</h3>
+                </div>
+                <div class="profession" style="--i:1;">
+                <i class='bx bxs-data' ></i>
+                <h3>ADMINISTRA</h3>
+                </div>
+                <div class="profession" style="--i:2;">
+                <i class='bx bxl-c-plus-plus' ></i>
+                <h3>DESARROLLA</h3>
+                </div>
+                <div class="profession" style="--i:3;">
+                <i class='bx bxs-file-html'></i>
+                <h3>DISEÑA</h3>
+                </div>
+
+                <div class="circle"></div>
+            </div>
+
+            <div class="overlay"></div>
+        </div>
+
+        <div class="home-img">
+            <img src="img/home.png" alt="">
+        </div>
     </section>
 
-    <section>
-        <h2>Desarrollo de aplicaciones móviles</h2>
-        <p>En la era de los smartphones, las aplicaciones móviles son una herramienta esencial para cualquier empresa. En Softix, desarrollamos aplicaciones móviles para iOS y Android que ayudan a nuestros clientes a llegar a su público objetivo de manera efectiva.</p>
+    <!-- acerca de nosotros -->
+    <section class="about" id="#">
+        <div class="about-img">
+            <img src="img/about.png" alt="">
+        </div>
+
+        <div class="about-content">
+            <h2 class="heading">Acerca de <span>Nosotros</span></h2>
+            <h3>Inovacion tecnologica a tu alcance</h3>
+            <p>En el mundo en constante evolución de la tecnología, Softix se destaca como un líder incuestionable. 
+                Fundada con una visión audaz en mente y una pasión por la innovación,Softix ha crecido para convertirse 
+                en una empresa de renombre en la industria tecnológica.</p>
+            <a href="#" class="btn">Leer mas</a>
+        </div>
     </section>
 
-    <section>
-        <h2>Desarrollo de sitios web</h2>
-        <p>Un sitio web es la cara de su empresa en línea. En Softix, creamos sitios web atractivos y funcionales que ayudan a nuestros clientes a conectarse con su audiencia en línea. Ya sea que necesite un sitio web de comercio electrónico o un sitio web informativo, podemos ayudarlo.</p>
+    <!-- Puestos -->
+    <section class="services" id="#">
+        <h2 class="heading">Nuestros <span>Campos</span></h2>
+
+        <div class="services-container">
+            <div class="services-box">
+                <i class='bx bx-code-block' ></i>
+                <h3>Desarrollador de software</h3>
+                <p>Soluciones personalizadas para satisfacer las necesidades comerciales específicas. Desarrollo de aplicaciones 
+                    de escritorio, web y móviles.</p>
+                <a href="#" class="btn">Leer mas</a>
+            </div>
+            <div class="services-box">
+                <i class='bx bxs-user-circle' ></i>
+                <h3>Diseñador de experiencia de usuario</h3>
+                <p>Creación de interfaces atractivas y funcionales que mejoran la satisfacción y 
+                    la usabilidad del usuario.</p>
+                <a href="#" class="btn">Leer mas</a>
+            </div>
+            <div class="services-box">
+                <i class='bx bx-devices' ></i>                
+                <h3>Desarrollador de aplicaciones moviles</h3>
+                <p>Desarrollo de aplicaciones móviles nativas y multiplataforma para iOS y Android, desde la concepción hasta 
+                    el lanzamiento.</p>
+                <a href="#" class="btn">Leer mas</a>
+            </div>
+            <div class="services-box">
+                <i class='bx bxs-help-circle' ></i>
+                <h3>Consultoria en tecnologia</h3>
+                <p>Asesoramiento experto para optimizar infraestructuras y estrategias tecnológicas, mejorando la eficiencia 
+                    empresarial.</p>
+                <a href="#" class="btn">Leer mas</a>
+            </div>
+        </div>
+    </section>
+    <!-- portafolio -->
+    <section class="portfolio" id="#">
+        <h2 class="heading">Ultimos <span>Proyectos</span></h2>
+        <div class="portfolio-container">
+            <div class="portfolio-box">
+                <img src="img/portfolio1.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src="img/portfolio2.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src="img/portfolio3.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src="img/portfolio4.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src="img/portfolio5.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src="img/portfolio6.jpg" alt="">
+                <div class="portfolio-layer">
+                    <h4>Diseño web</h4>
+                    <p>Gran creatividad</p>
+                    <a href="#"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <section>
-        <h2>Servicios de mantenimiento y soporte</h2>
-        <p>En Softix, no solo desarrollamos software, sino que también brindamos servicios de mantenimiento y soporte para garantizar que su software funcione sin problemas. Ofrecemos servicios de mantenimiento preventivo y correctivo, así como soporte técnico para ayudarlo a resolver cualquier problema que pueda surgir.</p>
+
+    <!-- Nuestro equipo -->
+    <div class="testimonial-container">
+        <h2 class="heading">Nuestro <span>Equipo</span></h2>
+        <div class="testimonial-wrapper">
+            <div class="testimonial-box mySwiper">
+                <div class="testimonial-content swiper-wrapper">
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="img/testimonial1.jpg" alt="">
+                        <h3>Juan Pablo de Luna de la Serna</h3>
+                        <p>Estudiante de la Universidad Autonoma de Aguascalientes</p>
+                    </div>
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="img/testimonial2.jpg" alt="">
+                        <h3>Johan Ronaldo Martinez Ramirez</h3>
+                        <p>Estudiante de la Universidad Autonoma de Aguascalientes</p>
+                    </div>
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="img/testimonial3.jpg" alt="">
+                        <h3>Uriel Ulises Acosta Olvera</h3>
+                        <p>Estudiante de la Universidad Autonoma de Aguascalientes</p>
+                    </div>
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="img/testimonial1.jpg" alt="">
+                        <h3>Johan Jose Maria renteria Zaragoza</h3>
+                        <p>Estudiante de la Universidad Autonoma de Aguascalientes</p>
+                    </div>
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- CONTACTO SECCION -->
+    <section class="contact" id="contacto">
+        <h2 class="heading">Quieres saber mas? <span>Contactanos!</span></h2>
+
+        <form action="#">
+            <div class="input-box">
+                <input type="text" placeholder="Nombre Completo">
+                <input type="email" placeholder="Correo electronico">
+            </div>
+            <div class="input-box">
+                <input type="number" placeholder="Numero Celular">
+                <input type="text" placeholder="Direccion">
+            </div>
+            <textarea name="" id="" cols="30" rows="10" placeholder="Tu mensaje"></textarea>
+            <input type="submit" placeholder="Enviar mensaje" class="btn">
+        </form>
     </section>
 
-    <?php require "footer.html" ?>
+    <!-- footer design -->
+    <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 por SOFTIX | Todos los Derechos Reservados</p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class='bx bx-up-arrow-alt' ></i></a>
+        </div>
+    </footer>
+
+    <!-- scroll reveal -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!-- swiper js -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- custom js -->
+    <script src="js/script.js"></script>
 </body>
 
 </html>
