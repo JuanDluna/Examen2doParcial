a1b04965e3ab4968f36694f38d6891366450d69f
uriel <uriel.ao@live.com>
2023-10-29 23:49:53 -0600

    Añadido verificaciones de registro
    Si se quiere añadir algún usuario con el mismo nombre y mismo correo no se permite

--- public_html/procesar_registro.php
+++ public_html/procesar_registro.php
@@ -1,17 +1,16 @@
 <?php
 
-    define("BLANK_SPACE", " ");
+define("BLANK_SPACE", " ");
 
-function userRegistration($name, $email, $password){
-
+function userRegistration($name, $email, $password) {
     $file = "accounts.txt";
     $handle = fopen($file, 'a');
 
-    if($handle === false)
+    if ($handle === false)
         die("Unable to access");
 
     $hash = password_hash($password, PASSWORD_BCRYPT);
-    
+
     fwrite($handle, $name . BLANK_SPACE);
     fwrite($handle, $hash . BLANK_SPACE);
     fwrite($handle, $email . PHP_EOL);
@@ -19,27 +18,46 @@ function userRegistration($name, $email, $password){
     fclose($handle);
 }
 
+function userExists($name, $email) {
     $file = "accounts.txt";
-    $handle = fopen($file, "r");
+    $handle = fopen($file, 'r');
 
-    if ($handle === false){
-
-        die("Unable to open file"); 
+    if ($handle === false) {
+        die("Unable to access");
     }
 
-    if($_SERVER["REQUEST_METHOD"] == 'POST'){
-        
-        $name = $_POST["username"];
-        $userEmail = $_POST["useremail"];
-        $userPassword = $_POST["password"];
+    while (!feof($handle)) {
+        $line = fgets($handle);
+        $userData = explode(BLANK_SPACE, $line);
 
-        userRegistration($name, $userEmail, $userPassword);
+        if (count($userData) >= 3) {
+            $storedName = $userData[0];
+            $storedEmail = trim($userData[2]);
+
+            if ($name === $storedName && $email === $storedEmail) {
+                fclose($handle);
+                return true; // El usuario ya existe
+            }
+        }
     }
-
-    $loginData = array();
-
 
     fclose($handle);
+    return false; // El usuario no existe
+}
 
-    header("Location: index.php");
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $name = $_POST["username"];
+    $userEmail = $_POST["useremail"];
+    $userPassword = $_POST["password"];
+
+    if (userExists($name, $userEmail)) {
+        // echo "El usuario ya existe. No se ha modificado el archivo.";
+    } else {
+        userRegistration($name, $userEmail, $userPassword);
+        // echo "Usuario registrado con éxito.";
+    }
+}
+
+header("Location: index.php");
+
 ?>
